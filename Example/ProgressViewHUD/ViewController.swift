5095611d94dcd1b49b6345dcc3fcee54a99edd92
Yosua Hoo <yosu4.hoo@gmail.com>
2018-08-08 17:13:29 +0700

    bikin lingkaran, dan background lingkaran

--- Example/ProgressViewHUD/ViewController.swift
+++ Example/ProgressViewHUD/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import ProgressViewHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("test")
+        show(progressValue: 100, maxValue: 200)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    let shapeLayer = CAShapeLayer()
+    
+    func show(progressValue : Float, maxValue : Float){
+        let percentage = progressValue / maxValue * 100
+        let radius =  percentage / 100 * 2
+        
+        let backgroundShapeLayer = CAShapeLayer()
+        let center = view.center
+        let backgroundCircularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        backgroundShapeLayer.path = backgroundCircularPath.cgPath
+        backgroundShapeLayer.strokeColor = UIColor.gray.cgColor
+        backgroundShapeLayer.lineWidth = 10
+        backgroundShapeLayer.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(backgroundShapeLayer)
+        
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: CGFloat(radius) * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        //shapeLayer.strokeEnd = CGFloat(percentage)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        //view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+    }
+    
+    @objc func handleTap(){
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 2
+        
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey : "basic")
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ ProgressViewHUD.swift
@@ -0,0 +1,43 @@
+//
+//  ProgressViewHUD.swift
+//  Pods-ProgressViewHUD_Example
+//
+//  Created by Yosua Hoo on 08/08/18.
+//
+
+import Foundation
+
+public class ProgressHUD : UIViewController{
+    
+    func show(progressValue : Float, maxValue : Float){
+        let percentage = progressValue / maxValue * 100
+        let radius =  percentage / 100 * 360
+        
+        let backgroundShapeLayer = CAShapeLayer()
+        let center = view.center
+        let backgroundCircularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        backgroundShapeLayer.path = backgroundCircularPath.cgPath
+        backgroundShapeLayer.strokeColor = UIColor.gray.cgColor
+        backgroundShapeLayer.lineWidth = 10
+        backgroundShapeLayer.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(backgroundShapeLayer)
+        
+        let shapeLayer = CAShapeLayer()
+        
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 0.5 * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        //shapeLayer.strokeEnd = 0
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        
+        view.layer.addSublayer(shapeLayer)
+        
+    }
+    
+}
